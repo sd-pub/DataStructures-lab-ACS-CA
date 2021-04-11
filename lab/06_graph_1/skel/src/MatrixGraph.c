7387037e2ee1519c09ca67bd801c31253fcd6b98
student <ramsimihai>
2021-04-12 02:34:43 +0300

    [lab06] Added solution for a graph implementation
    including:
    - implementation with matrix of adjacency
    - implementation with lists of neighbours
    - BFS
    - DFS
    - Floyd-Warshall

--- lab/06_graph_1/skel/src/MatrixGraph.c
+++ lab/06_graph_1/skel/src/MatrixGraph.c
@@ -1,32 +1,81 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "MatrixGraph.h"
+#include "utils.h"
 
 /**
  * Initializeaza graful cu numarul de noduri primit ca parametru si aloca
  * memorie pentru matricea de adiacenta a grafului.
-*/
-void init_matrix_graph(MatrixGraph *graph, int nodes) {
-    /* TODO */
+ */
+matrix_graph_t*
+mg_create(int nodes)
+{
+	matrix_graph_t *mg = malloc(sizeof(*mg));
+	DIE(NULL == mg, "malloc failed");
+
+	mg->matrix = malloc(nodes * sizeof(*mg->matrix));
+	DIE(NULL == mg->matrix, "malloc failed");
+
+	for (int i = 0; i < nodes; i++) {
+		mg->matrix[i] = calloc(nodes, sizeof(mg->matrix[i]));
+		DIE(NULL == mg->matrix[i], "malloc failed");
+		
+	}
+
+	mg->nodes = nodes;
+
+	return mg;
 }
 
 /* Adauga o muchie intre nodurile sursa si destinatie */
-void add_edge_matrix_graph(MatrixGraph *graph, int src, int dest) {
-    /* TODO */
+void
+mg_add_edge(matrix_graph_t* graph, int src, int dest)
+{
+	if (!graph || !graph->matrix)
+		return;
+	else if (src < 0 || src > graph->nodes)
+		return;
+	else if (dest < 0 || dest > graph->nodes)
+		return;
+	
+	graph->matrix[src][dest] = 1;
 }
 
 /* Returneaza 1 daca exista muchie intre cele doua noduri, 0 in caz contrar */
-int has_edge_matrix_graph(MatrixGraph *graph, int src, int dest) {
-    /* TODO */
+int
+mg_has_edge(matrix_graph_t* graph, int src, int dest)
+{
+	if (!graph)
+		return 0;
+	else if (src < 0 || src > graph->nodes)
+		return 0;
+	else if (dest < 0 || dest > graph->nodes)
+		return 0;
+
+	return graph->matrix[src][dest];
 }
 
 /* Elimina muchia dintre nodurile sursa si destinatie */
-void remove_edge_matrix_graph(MatrixGraph *graph, int src, int dest) {
-    /* TODO */
+void
+mg_remove_edge(matrix_graph_t* graph, int src, int dest)
+{
+	if (!graph || !graph->matrix)
+		return;
+	else if (src < 0 || src > graph->nodes)
+		return;
+	else if (dest < 0 || dest > graph->nodes)
+		return;
+	
+	graph->matrix[src][dest] = 0;
 }
 
 /* Elibereaza memoria folosita de matricea de adiacenta a grafului */
-void clear_matrix_graph(MatrixGraph *graph) {
-    /* TODO */
+void
+mg_free(matrix_graph_t* graph)
+{
+	for (int i =  0; i < graph->nodes; i++)
+		free(graph->matrix[i]);
+	free(graph->matrix);
+	free(graph);
 }
--- lab/06_graph_1/skel/src/Test.c
+++ lab/06_graph_1/skel/src/Test.c
@@ -1,60 +1,290 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ListGraph.h"
 #include "MatrixGraph.h"
+#include "Stack.h"
+#include "Queue.h"
 
 #define MAX_NODES 10
+#define MAX_INT 11500
+static int time;
 
-void print_matrix_graph(MatrixGraph *g) {
-    /* TODO */
+void
+print_matrix_graph(matrix_graph_t* mg)
+{
+	for (int i = 0; i < mg->nodes; i++) {
+		for (int j = 0; j < mg->nodes; j++)
+			fprintf(stdout, "%d ", mg->matrix[i][j]);
+		
+		fprintf(stdout, "\n");
+	}
 }
 
-void print_list_graph(ListGraph *g) {
-    /* TODO */
+void
+print_list_graph(list_graph_t* lg)
+{
+	for (int i = 0; i < lg->nodes; i++) {
+		printf("neighbours for %d are: ", i);
+		ll_print_int(lg->neighbours[i]);
+	}
+
 }
 
-void dfs_list_graph(ListGraph *lg, int node, int *color, int *t_desc, int *t_tin) {
-    /* TODO */
+void
+dfs_list_graph(list_graph_t* lg,
+		int node,
+		int* visited,
+		int* t_desc,
+		int* t_tin)
+{
+	time = 0;
+	stack_t *stack = st_create(sizeof(int));
+	memset(visited, 0, MAX_NODES * sizeof(int));
+
+	st_push(stack, &node);
+
+	while (!st_is_empty(stack)) {
+		int s = *(int *)st_peek(stack);
+		st_pop(stack);
+		if (!visited[s]) {
+			t_desc[s]= time++;
+			printf("%d ", s);
+			visited[s] = 1;
+
+		}
+
+		ll_node_t *current = lg_get_neighbours(lg, s)->head;
+		
+		while (current) {
+			int next = *(int *)current->data;
+			if (!visited[next])
+				st_push(stack, &next);
+
+			current = current->next;
+		}
+		t_tin[s] = time++;
+	}
+
+	printf("\n");
+	st_free(stack);
 }
 
-void dfs_matrix_graph(MatrixGraph *lg, int node, int *color, int *t_desc, int *t_tin) {
-    /* TODO */
+void
+dfs_matrix_graph(matrix_graph_t* mg,
+		 int node,
+		 int* visited,
+		 int* t_desc,
+		 int* t_tin)
+{
+	if (visited[node]) 
+		return;
+
+	visited[node] = 1;
+	t_desc[node] = time++;
+	printf("%d ", node);
+	
+	for (int i = 0; i < mg->nodes; i++)
+		if (mg->matrix[node][i]) {
+			if (!visited[i])
+				dfs_matrix_graph(mg, i, visited, t_desc, t_tin);
+		}
+
+	t_desc[node] = time++;
 }
 
-void bfs_list_graph(ListGraph *lg, int node, int *color, int *parent) {
-    /* TODO */
+void
+bfs_list_graph(list_graph_t* lg, int node, int* visited)
+{
+	time = 0;
+	queue_t *q = q_create(sizeof(int), MAX_NODES);
+	memset(visited, 0, MAX_NODES * sizeof(int));
+
+	q_enqueue(q, &node);
+
+	while (!q_is_empty(q)) {
+		int s = *(int *)q_front(q);
+		q_dequeue(q);
+		if (!visited[s]) {
+			printf("%d ", s);
+			visited[s] = 1;
+
+		}
+
+		ll_node_t *current = lg_get_neighbours(lg, s)->head;
+		
+		while (current) {
+			int next = *(int *)current->data;
+			if (!visited[next])
+				q_enqueue(q, &next);
+
+			current = current->next;
+		}
+	}
+
+	printf("\n");
+
+	q_free(q);
 }
 
-void bfs_matrix_graph(MatrixGraph *lg, int node, int *color, int *parent) {
-    /* TODO */
+void
+bfs_matrix_graph(matrix_graph_t* mg, int node, int* visited)
+{
+	queue_t *q = q_create(sizeof(int), MAX_NODES);
+
+	visited[node] = 1;
+	q_enqueue(q, &node);
+
+	while (!q_is_empty(q)) {
+		int s = *(int *)q_front(q);
+		q_dequeue(q);
+		printf("%d ", s);
+		
+
+		for (int i = 0; i < mg->nodes; i++) {
+			if (mg->matrix[s][i] && !visited[i]) {
+				visited[i] = 1;
+				q_enqueue(q, &i);
+			}
+		}
+	}
+	
+	printf("\n");
+	q_free(q);
 }
 
-void floyd_warshall(MatrixGraph *lg) {
-    /* TODO BONUS */
+void
+floyd_warshall(matrix_graph_t* mg)
+{
+	for (int i = 0; i < mg->nodes; i++) {
+		for (int j = 0; j < mg->nodes; j++) {
+			if (mg->matrix[i][j] == 0 && i != j)
+				mg->matrix[i][j] = MAX_INT;
+		}
+	}
+
+	for (int k = 0; k < mg->nodes; k++) {
+    	for (int i = 0; i < mg->nodes; i++) {
+        	for (int j = 0; j < mg->nodes; j++) {
+				if (mg->matrix[i][j] > mg->matrix[i][k] + mg->matrix[k][j])
+					mg->matrix[i][j] = mg->matrix[i][k] + mg->matrix[k][j];
+        	}
+   		}
+	}
+
+	for (int i = 0; i < mg->nodes; i++) {
+		for (int j = 0; j < mg->nodes; j++) {
+			if (mg->matrix[i][j] == MAX_INT && i != j)
+				mg->matrix[i][j] = 0;
+		}
+	}
 }
 
-int main() {
-    int nodes, edges;
-    int x[MAX_NODES], y[MAX_NODES];
-    int color[MAX_NODES], t_dest[MAX_NODES], t_fin[MAX_NODES];
-    ListGraph *lg = malloc(sizeof(ListGraph));
-    MatrixGraph *mg = malloc(sizeof(MatrixGraph));
+int
+main()
+{
+	int visited[MAX_NODES], t_dest[MAX_NODES], t_fin[MAX_NODES];
+	list_graph_t* lg = lg_create(MAX_NODES);
+	matrix_graph_t* mg = mg_create(MAX_NODES);
 
-    scanf("%d %d", &nodes, &edges);
 
-    init_list_graph(lg, nodes);
-    init_matrix_graph(mg, nodes);
+	int numbers[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    for (int i = 0; i < edges; ++i) {
-        scanf("%d %d", &x[i], &y[i]);
-        add_edge_matrix_graph(mg, x[i], y[i]);
-        add_edge_list_graph(lg, x[i], &y[i]);
-    }
+	printf("-------------------------------- Test ListGraph "
+		"--------------------------------\n");
+	/* Test add_edge_list_graph has_edge_list_graph */
 
-    clear_list_graph(lg);
-    clear_matrix_graph(mg);
-    free(lg);
-    free(mg);
-    return 0;
+	lg_add_edge(lg, numbers[0], numbers[1]);
+	lg_add_edge(lg, numbers[3], numbers[2]);
+
+	printf("#1:\nOutput: %d\nOutput asteptat: 1\n",
+		lg_has_edge(lg, numbers[0], numbers[1]));
+
+	printf("#2:\nOutput: %d\nOutput asteptat: 1\n",
+		lg_has_edge(lg, numbers[3], numbers[2]));
+
+	printf("#3:\nOutput: %d\nOutput asteptat: 0\n",
+		lg_has_edge(lg, numbers[2], numbers[4]));
+
+	/* Test remove_edge_list_graph */
+	lg_remove_edge(lg, numbers[0], numbers[1]);
+	printf("#4:\nOutput: %d\nOutput asteptat: 0\n",
+		lg_has_edge(lg, numbers[0], numbers[1]));
+
+	/* Test get_neighbours_list_graph */
+	lg_add_edge(lg, numbers[0], numbers[1]);
+	lg_add_edge(lg, numbers[0], numbers[2]);
+	lg_add_edge(lg, numbers[0], numbers[3]);
+	lg_add_edge(lg, numbers[0], numbers[4]);
+
+	linked_list_t* l = lg_get_neighbours(lg, numbers[0]);
+
+	printf("#5:\n");
+	printf("Output: ");
+	ll_print_int(l);
+	printf("Output asteptat: 1 2 3 4\n");
+
+	printf("-------------------------------- Test MatrixGraph "
+		"--------------------------------\n");
+	/* test add edge_matrix_graph has_edge_matrix_graph */
+	mg_add_edge(mg, numbers[0], numbers[1]);
+	mg_add_edge(mg, numbers[3], numbers[2]);
+
+	printf("#1:\nOutput: %d\nOutput asteptat: 1\n",
+		mg_has_edge(mg, numbers[0], numbers[1]));
+
+	printf("#2:\nOutput: %d\nOutput asteptat: 1\n",
+		mg_has_edge(mg, numbers[3], numbers[2]));
+
+	printf("#3:\nOutput: %d\nOutput asteptat: 0\n",
+		mg_has_edge(mg, numbers[2], numbers[4]));
+
+	/* test remove_edge_matrix_graph */
+	mg_remove_edge(mg, numbers[0], numbers[1]);
+	printf("#4:\nOutput: %d\nOutput asteptat: 0\n",
+		mg_has_edge(mg, numbers[0], numbers[1]));
+	
+	mg_add_edge(mg, numbers[0], numbers[1]);
+	mg_add_edge(mg, numbers[0], numbers[2]);
+	mg_add_edge(mg, numbers[0], numbers[3]);
+	mg_add_edge(mg, numbers[2], numbers[4]);
+
+	// print_list_graph(lg);
+	// print_matrix_graph(mg);
+
+	dfs_list_graph(lg, numbers[0], visited, t_dest, t_fin);
+	bfs_list_graph(lg, numbers[0], visited);
+	
+	memset(visited, 0, MAX_NODES * sizeof(int));
+	time = 0;
+	dfs_matrix_graph(mg, numbers[0], visited, t_dest, t_fin);
+	printf("\n");
+
+	memset(visited, 0, MAX_NODES * sizeof(int));
+	bfs_matrix_graph(mg, numbers[0], visited);
+	
+	// Matrice random de testare
+	int cnt = 1;
+	for (int i = 0; i < mg->nodes; i++) {
+		for (int j = 0; j < mg->nodes; j++) {
+			if (mg->matrix[i][j] == 1) {
+				mg->matrix[i][j] = cnt;
+				cnt++;
+			}
+		}
+			
+	}
+
+	printf("Matricea inainte de FW\n");
+	print_matrix_graph(mg);
+
+	floyd_warshall(mg);
+	
+	printf("Matricea dupa FW\n");
+	print_matrix_graph(mg);
+
+	lg_free(lg);
+	mg_free(mg);
+	return 0;
 }
